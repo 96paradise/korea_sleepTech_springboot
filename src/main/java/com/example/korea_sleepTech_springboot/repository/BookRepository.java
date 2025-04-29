871ae80bc8045ac9470396e408d0a5667f8e3a04
96paradise <cnw0806@naver.com>
2025-04-29 16:26:37 +0900

    20250429 CNW

--- src/main/java/com/example/korea_sleepTech_springboot/repository/BookRepository.java
+++ src/main/java/com/example/korea_sleepTech_springboot/repository/BookRepository.java
@@ -1,4 +1,9 @@
 package com.example.korea_sleepTech_springboot.repository;
 
-public interface BookRepository {
+import com.example.korea_sleepTech_springboot.entity.C_Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<C_Book, Long> {
 }
