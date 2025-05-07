319719aa6330c5749caff258465b941b514bc31e
96paradise <cnw0806@naver.com>
2025-05-07 16:45:04 +0900

    20250507 CNW

---
+++ src/main/java/com/example/korea_sleepTech_springboot/service/PostService.java
@@ -0,0 +1,14 @@
+package com.example.korea_sleepTech_springboot.service;
+
+import com.example.korea_sleepTech_springboot.dto.request.PostCreateRequestDto;
+import com.example.korea_sleepTech_springboot.dto.response.PostDetailResponseDto;
+import com.example.korea_sleepTech_springboot.dto.response.ResponseDto;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    public ResponseDto<PostDetailResponseDto> createPost(@Valid PostCreateRequestDto dto) {
+        return null;
+    }
+}
