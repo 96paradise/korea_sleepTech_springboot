7b7ae2109e6ab1b839260dbbcf4aa9ca6daa6acd
96paradise <cnw0806@naver.com>
2025-06-03 17:12:00 +0900

    20250603 PT CNW

--- src/main/java/com/example/korea_sleepTech_springboot/teamproject/entity/Subject.java
+++ src/main/java/com/example/korea_sleepTech_springboot/teamproject/entity/Subject.java
@@ -1,4 +1,43 @@
 package com.example.korea_sleepTech_springboot.teamproject.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Subject {
+    @Id
+    private String subjectId;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id")
+    private School school;
+
+    private String subjectName;
+    private String grade;
+    private String semester;
+
+    @Enumerated(EnumType.STRING)
+    private Affiliation affiliation;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private Integer maxEnrollment;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public enum Affiliation {
+        LIBERAL_ARTS, NATURAL_SCIENCES
+    }
+
+    public enum Category {
+        COMPLETED, NOT_SELECTED
+    }
 }
